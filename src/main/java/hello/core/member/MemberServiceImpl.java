8fe1ab81bce89496cc084d6d92ace4b80382f9c7
sein <lsi6601@gmail.com>
2023-10-03 16:36:56 +0900

    옮기기

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,34 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+//    변경 전
+//    ** DIP위반 : MemoryMemberRepository() 구현 클래스에도 의존하고 있다.
+//    ** OCP위반 : 지금 코드는 기능을 확장해서 변경하면, 클라이언트 코드에도 영향을 준다.
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+//    변경 후
+//    ** DIP : MemberRepository 인터페이스에만 의존한다.
+//    MemberServiceImpl는 어떤 구현 객체가 들어올지 알 수 없다.
+//      -> 생성자를 통ㅇ해서 어떤 구현 객체가 들어올지는 오직 위부(AppConfig)에서 결정된다.
+//      -> 의존관계에 대한 고민은 외부에 맡기고 실행헤만 집중하면 된다.
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+
+
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,30 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+//    MemberService memberService = new MemberServiceImpl(memberRepository);
+
+    MemberService memberService;
+
+    @BeforeEach // @BeforeEach : Test 전에 꼭 실행되는 것.. Test가 2개 있으면 2번 돌아감..
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
