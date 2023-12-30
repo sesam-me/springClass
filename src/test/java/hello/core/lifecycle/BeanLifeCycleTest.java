6bc3ba8e7f8b424399033f7398f0ecec1a1ddac4
sesam-me <lsi6601@gmail.com>
2023-12-31 03:55:44 +0900

    section 8 : 빈 생명주기 콜백

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -1,8 +1,6 @@
 package hello.core.lifecycle;
 
-import net.bytebuddy.utility.nullability.MaybeNull;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +17,7 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig {
+//        @Bean(initMethod = "init", destroyMethod = "close")
         @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
